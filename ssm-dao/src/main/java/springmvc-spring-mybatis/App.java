4f7dd9b27d9fd0b957b854df4cb95b3a02d3c6f6
zxy <842675355@qq.com>
2019-01-14 10:35:23 +0800

    first commit

---
+++ ssm-dao/src/main/java/springmvc-spring-mybatis/App.java
@@ -0,0 +1,13 @@
+package springmvc-spring-mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
